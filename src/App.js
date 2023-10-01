14b36387a6de35de42ea6e40ddcd8e2e840e10ee
haku-ohsumi <haku818181@gmail.com>
2023-10-01 23:03:39 +0900

    アイテム作成できるよ

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Register from"./pages/user/register"
 import Login from"./pages/user/login"
 import ReadAll from "./pages/item/readAll"
 import ReadSingleItem from "./pages/item/readSingle"
+import CreateItem from "./pages/item/create"
 
 import "./App.css"
 
@@ -16,6 +17,7 @@ const App = () => {
         <Route path="/user/login" element={<Login/>}/>
         <Route path="/" element={<ReadAll/>}/>
         <Route path="/item/:id" element={<ReadSingleItem/>}/>
+        <Route path="/item/create" element={<CreateItem/>}/>
       </Routes>
     </BrowserRouter>
   )
