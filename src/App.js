c3dcc818385ed7db2bf4dffe6a2b7f8cfc4a3965
haku-ohsumi <haku818181@gmail.com>
2023-10-13 11:04:00 +0900

    管理者登録を複製、AdminUser→Staffに変更

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes, BrowserRouter} from "react-router-dom"
 
 import AdminLogin from"./pages/admin/user/login"
 import AdminRegister from"./pages/admin/user/register"
+import StaffRegister from"./pages/admin/staff/register"
 import Header from "./components/header"
 import Footer from "./components/footer"
 
@@ -16,6 +17,7 @@ const App = () => {
       <Routes>
         <Route path="/admin/user/login" element={<AdminLogin/>}/>
         <Route path="/admin/user/register" element={<AdminRegister/>}/>
+        <Route path="/admin/staff/register" element={<StaffRegister/>}/>
       </Routes>
       <Footer/>
       </div>
