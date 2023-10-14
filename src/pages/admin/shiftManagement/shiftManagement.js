18cd81f316d369f05ae02fae9e11377d661dea4c
haku-ohsumi <haku818181@gmail.com>
2023-10-14 16:04:38 +0900

    入力フォームを作成

--- src/pages/admin/shiftManagement/shiftManagement.js
+++ src/pages/admin/shiftManagement/shiftManagement.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
@@ -6,41 +6,88 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 const localizer = momentLocalizer(moment);
 
 const ShiftManagement = () => {
-  const [events, setEvents] = useState([]); // シフトのイベントリスト
+  const [events, setEvents] = useState([]);
+  const [newShift, setNewShift] = useState({ title: "", start: null, end: null });
 
-  const handleSelect = (slotInfo) => {
-    // 新しいシフトを追加
-    const newEvent = {
-      title: "新しいシフト",
-      start: slotInfo.start,
-      end: slotInfo.end,
-    };
+  
+  useEffect(() => {
+    // バックエンドからシフトデータを取得し、setEvents で更新
+    // 例: fetchShifts().then((data) => setEvents(data));
+  }, []);
 
-    setEvents([...events, newEvent]);
+  const handleAddShift = () => {
+    if (newShift.start && newShift.end) {
+      // 新しいシフトをバックエンドに送信
+      fetch("http://localhost:5100/shifts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newShift),
+      })
+        .then((response) => {
+          if (response.status === 200) {
+            return response.json();
+          } else {
+            throw new Error("Failed to create shift.");
+          }
+        })
+        .then((createdShift) => {
+          // シフトが正常に作成された場合、events ステートを更新
+          setEvents([...events, createdShift]);
+          setNewShift({ title: "", start: null, end: null });
+        })
+        .catch((error) => {
+          console.error("Error creating shift:", error);
+        });
+    }
   };
 
   const handleEventRemove = (event) => {
-    // シフトを削除
-    const updatedEvents = events.filter((e) => e !== event);
-    setEvents(updatedEvents);
+    // シフトを削除し、バックエンドに送信
+    // 例: deleteShift(event.id).then(() => {
+    //        const updatedEvents = events.filter((e) => e.id !== event.id);
+    //        setEvents(updatedEvents);
+    //      });
   };
 
   return (
     <div>
       <h1>シフト管理</h1>
+      <div>
+        <h2>新しいシフトを追加</h2>
+        <div>
+          <label>開始時間:</label>
+          <input
+            type="datetime-local"
+            value={newShift.start}
+            onChange={(e) => setNewShift({ ...newShift, start: e.target.value })}
+          />
+        </div>
+        <div>
+          <label>終了時間:</label>
+          <input
+            type="datetime-local"
+            value={newShift.end}
+            onChange={(e) => setNewShift({ ...newShift, end: e.target.value })}
+          />
+        </div>
+        <button onClick={handleAddShift}>シフトを追加</button>
+      </div>
       <Calendar
         localizer={localizer}
         events={events}
         startAccessor="start"
         endAccessor="end"
         selectable
-        onSelectSlot={handleSelect}
+        onSelectSlot={handleAddShift}
         eventPropGetter={(event) => ({
           style: {
             backgroundColor: "#0074D9",
           },
         })}
         onSelectEvent={handleEventRemove}
+        view={['week']}  // 週表示のみを許可
       />
     </div>
   );
