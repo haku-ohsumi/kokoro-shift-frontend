74b2386855b0bd233aefebb561ce66794f5d718b
haku-ohsumi <haku818181@gmail.com>
2023-10-16 23:58:14 +0900

    スタッフ名をクリックできるようにする

--- src/pages/admin/staff/staffSelect.js
+++ src/pages/admin/staff/staffSelect.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function StaffSelect() {
   const [staffUsers, setStaffUsers] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetch("http://localhost:5100/api/staffUsers") // バックエンドのエンドポイントにGETリクエストを送信
@@ -10,12 +12,19 @@ function StaffSelect() {
       .catch((error) => console.error("データの取得に失敗しました", error));
   }, []);
 
+  const handleStaffClick = (staffId) => {
+    // スタッフ名をクリックした際のハンドラー
+    navigate("/admin/staffId/shift-management");
+  };
+
   return (
     <div>
       <h1>スタッフユーザー一覧</h1>
       <ul>
         {staffUsers.map((staffUser) => (
-          <li key={staffUser._id}>{staffUser.name}</li>
+          <li key={staffUser._id} onClick={() => handleStaffClick(staffUser._id)}>
+          {staffUser.name}
+        </li>
         ))}
       </ul>
     </div>
