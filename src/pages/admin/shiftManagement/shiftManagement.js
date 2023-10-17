b8067fb7910f1b3554d034ebfb56a39ed4345381
haku-ohsumi <haku818181@gmail.com>
2023-10-17 15:29:10 +0900

    カレンダーをライブラリーを用いて表示

--- src/pages/admin/shiftManagement/shiftManagement.js
+++ src/pages/admin/shiftManagement/shiftManagement.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 function ShiftForm() {
   const [startTime, setStartTime] = useState('');
   const [endTime, setEndTime] = useState('');
+  const [events, setEvents] = useState([]); 
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
@@ -16,6 +19,11 @@ function ShiftForm() {
       return;
     }
 
+    // カレンダーのイベントを更新
+    setEvents([...events, { title: 'Shift', start: startTime, end: endTime }]);
+    
+    setStartTime('');
+    setEndTime('');
 
     try {
       const response = await fetch(`http://localhost:5100/admin/${staffIdAdmin}/shift-management`, {
@@ -37,25 +45,34 @@ function ShiftForm() {
   };
 
   return (
-    <form onSubmit={handleFormSubmit}>
-      <div>
-        <label>勤務開始予定時間:</label>
-        <input
-          type="datetime-local"
-          value={startTime}
-          onChange={(e) => setStartTime(e.target.value)}
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          <label>勤務開始予定時間:</label>
+          <input
+            type="datetime-local"
+            value={startTime}
+            onChange={(e) => setStartTime(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>勤務終了予定時間:</label>
+          <input
+            type="datetime-local"
+            value={endTime}
+            onChange={(e) => setEndTime(e.target.value)}
+          />
+        </div>
+        <button type="submit">保存</button>
+      </form>
+        <div>
+        <FullCalendar
+          plugins={[dayGridPlugin]}
+          initialView="dayGridMonth"
+          events={events}
         />
-      </div>
-      <div>
-        <label>勤務終了予定時間:</label>
-        <input
-          type="datetime-local"
-          value={endTime}
-          onChange={(e) => setEndTime(e.target.value)}
-        />
-      </div>
-      <button type="submit">保存</button>
-    </form>
+        </div>
+    </div>
   );
 }
 
