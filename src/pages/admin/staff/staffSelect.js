35bf12b2d03e29ced6f4e49cecde7d6faf194f90
haku-ohsumi <haku818181@gmail.com>
2023-10-23 17:43:56 +0900

    時給アップ登録

--- src/pages/admin/staff/staffSelect.js
+++ src/pages/admin/staff/staffSelect.js
@@ -18,6 +18,31 @@ function StaffSelect() {
     navigate("/admin/staffId/shift-management");
   };
 
+  // ココロシフト時給アップ登録
+  const [wageUp, setWageUp] = useState(100);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    try {
+      const response = await fetch("http://localhost:5100/api/wage", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ wageUp }), // ここでデータを送信
+      });
+
+      if (response.ok) {
+        alert("ココロシフト時給アップが登録されました");
+      } else {
+        alert("ココロシフト時給アップの登録に失敗しました");
+      }
+    } catch (error) {
+      alert("エラー:", error);
+    }
+  };
+
   return (
     <div>
       <h1>スタッフユーザー一覧</h1>
@@ -28,6 +53,18 @@ function StaffSelect() {
         </li>
         ))}
       </ul>
+      <h2>ココロシフト時給アップ登録</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          ココロシフト時給アップ金額（円 / h）
+          <input
+            type="number"
+            value={wageUp}
+            onChange={(e) => setWageUp(e.target.value)}
+          />
+        </label>
+        <button type="submit">登録</button>
+      </form>
     </div>
   );
 }
